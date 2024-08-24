a5e739656eea2b949741c87a8b7be703114aa927
seemywings <kevin.jayne@icloud.com>
2024-08-24 15:20:08 -0400

    dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,130 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import SystemSettings from './settings/SystemSettings';
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SettingsSelector from './selectors/SettingsSelector';
+import { Box } from '@mui/material';
 
-var defaultConfig = {
-  "version": "1.0.0",
-  "mode": "client",
-  "mdns": "litbox",
-  "client": {
-    "ssid": "connectedness",
-    "password": "ReallyLongPassword123!@#"
+// Define the theme
+const theme = createTheme({
+
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
   },
-  "ap": {
-    "ssid": "LitBox",
-    "password": "abcd1234"
+
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: "#38ffb9",
+    },
   },
-  "brightness": 9,
-  "sensitivity": 9,
-  "visualization": "bars",
-  "frameRate": 30,
-  "temperatureUnit": "C",
-  "colorPallet": ['#0000FF', '#00FFFF', '#FF00D5', '#FFFFFF'],
-  pixelBgColor: "#000000",
-  pixelColor: "#f5f0f0",
-  "text": {
-    "content": "*.*. Lit Box .*.*",
-    "animation": "scroll",
-    "speed": "75",
-    "size": "1"
-  }
-};
+
+});
 
 function App() {
-  var [config, setConfig] = useState(null);
-  const [selectedSetting, setSelectedSetting] = useState('bars');  // Default to 'about'
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      setConfig(defaultConfig);
-      setSelectedSetting(defaultConfig.visualization);
-    } else {
-      fetch('/config')
-        .then((response) => response.json())
-        .then((data) => {
-          setConfig(data);
-          setSelectedSetting(data.visualization);
-        })
-        .catch((error) => console.error('Error loading configuration:', error));
-    }
-  }, []);
-
-  const saveConfig = (newConfig) => {
-    fetch('/saveConfig')
-      .then(response => {
-        if (!response.ok) throw new Error('Failed to save configuration');
-        return response.json();
-      }).then(data => {
-        setConfig(newConfig);
-        console.log('Configuration updated:', data);
-        alert('Configuration Saved');
-      }).catch(error => console.error('Error updating configuration:', error));
-  };
-
-  const updateConfig = (newConfig) => {
-    fetch('/config', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newConfig),
-    }).then(response => {
-      if (!response.ok) throw new Error('Failed to update configuration');
-      return response.json();
-    }).then(data => {
-      setConfig(newConfig);
-      console.log('Configuration Returned:', data);
-      // alert('Configuration updated');
-    }).catch(error => console.error('Error updating configuration:', error));
-  };
-
-  if (!config) {
-    return (
-      <div className="container">
-        <label className="header">Lit Box</label>
-        <div>Loading configuration... ⚙️</div>
-      </div>
-    );
-  }
-
-  const renderSetting = () => {
-    switch (selectedSetting) {
-      case 'system':
-        return <SystemSettings config={config} updateConfig={updateConfig} saveConfig={saveConfig} />;
-      case 'about':
-        return <div class="setting" id="about-settings">
-
-          <div class="setting">
-            <label>Version</label>
-            <label id="version">{config.version}</label>
-          </div>
-
-          <div class="setting">
-            <label>Designed and Developed by:</label>
-            <a href="https://github.com/seemywingz/vmon"
-              target="_blank" id="SeeMyWingZ" rel="noreferrer">SeeMyWingZ</a>
-          </div>
-
-        </div>;
-      default:
-        return <div>🛠️ Under Construction 🛠️</div>;
-    }
-  };
-
   return (
-    <div className="container">
-      <label className="header">VMON</label>
-      <div className="setting">
-        <select
-          id="settingSelect"
-          className="clickable"
-          value={selectedSetting}
-          onChange={(e) => setSelectedSetting(e.target.value)}
-        >
-          <option value="system">System</option>
-          <option value="about">About</option>
-        </select>
-      </div>
-      {renderSetting()}
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box // Header
+        sx={{
+          height: '9vh',
+          // color: 'primary.main',
+          backgroundColor: '#333',
+          textAlign: 'center',
+          zIndex: 1000,
+          fontSize: '3em',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        VMON
+      </Box>
+      <SettingsSelector />
+    </ThemeProvider>
   );
 }
-
 export default App;
--- dashboard/src/settings/SystemSettings.js
+++ dashboard/src/settings/SystemSettings.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
 
 function SystemSettings({ config, updateConfig, saveConfig }) {
+    const [localConfig, setLocalConfig] = useState(config);
 
+    const handleRestart = () => {
+        // Perform restart system action
+        alert("Restarting the system...");
+    }
 
     return (
-        <div class="setting">
-            <button id="redButton" onClick={() => { alert("Restarting") }} >Restart</button>
+        <div className="container">
+            <div className='setting'>
+                <Button variant="contained" color="primary" onClick={handleRestart}>
+                    Restart
+                </Button>
+            </div>
         </div >
     );
 }
