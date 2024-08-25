8fc4824c4516b3e463f5c89acfffaf718c9e88f3
Kevin Jayne <kevin.jayne@icloud.com>
2024-08-25 15:39:31 -0400

    app use env

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,7 +6,6 @@ import Pins from './pins/Pins';
 
 // Define the theme
 const theme = createTheme({
-
   typography: {
     fontFamily: [
       '-apple-system',
@@ -21,70 +20,75 @@ const theme = createTheme({
       '"Segoe UI Symbol"',
     ].join(','),
   },
-
   palette: {
     mode: 'dark',
     primary: {
       main: "#38ffb9",
     },
   },
-
 });
 
-var defaultConfig = {
-  "devport": 3000,
-  "dir": "./srv",
-  "hostname": "10.0.0.59",
-  "pins": [
+const defaultConfig = {
+  devport: 3000,
+  dir: "./srv",
+  hostname: "10.0.0.59",
+  pins: [
     {
-      "name": "Light 1",
-      "on": false,
-      "pin": 16
+      name: "Light 1",
+      on: false,
+      pin: 16,
     },
     {
-      "name": "Light 2",
-      "on": false,
-      "pin": 12
-    }
+      name: "Light 2",
+      on: false,
+      pin: 12,
+    },
   ],
-  "port": 8080
-}
-
+  port: 8080,
+};
 
 function App() {
-  var [config, setConfig] = useState('');
-  var hostname = "10.0.0.59"
-  var port = 8080
+  const [config, setConfig] = useState(null); // Initialize state with null
+  const hostname = process.env.API_HOST || "10.0.0.59"; // Fix typo and add fallback
+  const port = process.env.API_PORT || 8080;
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
       console.log("Using default configuration");
       setConfig(defaultConfig);
     } else {
-      fetch("http://" + hostname + ":" + port + "/api/config")
-        .then((response) => response.json())
+      fetch(`http://${hostname}:${port}/api/config`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
         .then((data) => {
           setConfig(data);
-          console.log("config", data);
+          console.log("Config:", data);
         })
         .catch((error) => console.error('Error loading configuration:', error));
     }
-  }, []);
-
+  }, [hostname, port]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Setting content={"VMON"} sx={{
-        backgroundColor: '#333',
-        color: 'primary.main',
-        fontSize: '3em',
-        height: '9vh',
-        zIndex: 1000,
-      }} />
+      <Setting
+        content={"VMON"}
+        sx={{
+          backgroundColor: '#333',
+          color: 'primary.main',
+          fontSize: '3em',
+          height: '9vh',
+          zIndex: 1000,
+        }}
+      />
       <Container>
         <Pins config={config} />
       </Container>
     </ThemeProvider>
   );
 }
+
 export default App;
